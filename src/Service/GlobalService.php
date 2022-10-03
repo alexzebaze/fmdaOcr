c05ea7dbd04728afc19e5ae421f0a223327003a6
alexzebaze <alexngoumo.an@gmail.com>
2022-10-03 10:29:07 +0100

    edit

--- src/Service/GlobalService.php
+++ src/Service/GlobalService.php
@@ -876,10 +876,10 @@ class GlobalService{
     public function cronOcrIa($entreprise = null){
 
         if(!is_null($entreprise)){
-            $documents = $this->em->getRepository(EmailDocumentPreview::class)->findBy(['execute'=>false, 'extension'=>'pdf', 'entreprise'=>$entreprise->getId(), 'sender'=>"amarkey@trenois.com"], ['id'=>"ASC"], 10);
+            $documents = $this->em->getRepository(EmailDocumentPreview::class)->findBy(['execute'=>false, 'extension'=>'pdf', 'entreprise'=>$entreprise->getId(), 'sender'=>"Contact@fmda.fr"], ['id'=>"ASC"], 10);
         }
         else{
-            $documents = $this->em->getRepository(EmailDocumentPreview::class)->findBy(['execute'=>false, 'extension'=>'pdf', 'sender'=>"amarkey@trenois.com"], ['id'=>"ASC"], 10);
+            $documents = $this->em->getRepository(EmailDocumentPreview::class)->findBy(['execute'=>false, 'extension'=>'pdf', 'sender'=>"Contact@fmda.fr"], ['id'=>"ASC"], 10);
         }
 
         $documentToRotate = [];
