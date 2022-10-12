bea96bdc2e03e5116db9f6b39217bc5427d1aa36
alexzebaze <alexngoumo.an@gmail.com>
2022-10-12 13:22:44 +0100

    edit

--- src/Service/GlobalService.php
+++ src/Service/GlobalService.php
@@ -2141,7 +2141,9 @@ class GlobalService{
     public function getResultIaLaunch($imgname, $dossier, $dirLandingImg, $entreprise = null){
 
         $tmpOcrFirst = $this->em->getRepository(TmpOcr::class)->getByParam($dossier, $imgname, "ASC", "LINE", 50, $entreprise->getId());
-        $tmpOcrLast = $this->em->getRepository(TmpOcr::class)->getByParam($dossier, $imgname, "DESC", "LINE", 50, $entreprise->getId());
+        
+        $tmpOcrLast = [];
+        //$tmpOcrLast = $this->em->getRepository(TmpOcr::class)->getByParam($dossier, $imgname, "DESC", "LINE", 50, $entreprise->getId());
 
         $tmpOcr = array_unique(array_merge($tmpOcrLast, $tmpOcrFirst), SORT_REGULAR);
 
