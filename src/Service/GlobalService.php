56d005e30dfc039d5f475f1b599d24d1ef1430e3
alexzebaze <alexngoumo.an@gmail.com>
2022-11-07 20:10:13 +0100

    edit

--- src/Service/GlobalService.php
+++ src/Service/GlobalService.php
@@ -2647,7 +2647,7 @@ class GlobalService{
             $dossier=>$entity,
             'fournisseurfound'=>$fournisseurfound,
             'chantierfound'=>$chantierfound,
-            'tvaVal'=>$tvaVal."%",
+            'tvaVal'=>round($tvaVal,2)."%",
             'oldDate'=>$oldDate,
             'color'=>$color,
             'clientfound'=>$clientfound,
